209ddf930d661354f33e7810e4d33cfa16ad4876
Aleksey Sviridkin <f@lex.la>
2024-03-27 23:10:39 +0400

    Add syncOptions CreateNamespace=true to argocd YAML files
    Signed-off-by: Aleksey Sviridkin <f@lex.la>

--- tools/kill-kube-ns.sh
+++ tools/kill-kube-ns.sh
@@ -41,6 +41,4 @@ trap killproxy EXIT
 
 sleep 1 # give the proxy a second
 
-kubectl get namespace "$PROJECT" -o json | jq 'del(.spec.finalizers[] | select("kubernetes"))' | curl -s -k -H "Content-Type: application/json" -X PUT -o /dev/null --data-binary @- http://localhost:8001/api/v1/namespaces/$PROJECT/finalize && echo "Killed namespace: $PROJECT"
-
-# proxy will get killed by the trap
+kubectl get namespace "$PROJECT" -o json | jq 'del(.spec.finalizers[] | select("kubernetes"))' | curl -s -k -H "Content-Type: application/json" -X PUT -o /dev/null --data-binary @- "http://localhost:8001/api/v1/namespaces/$PROJECT/finalize" && echo "Killed namespace: $PROJECT"
